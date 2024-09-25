9b66f49d0fade031d43286faaf8928a47f58025d
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2024-09-25 22:57:34 +0200

    Services lifetime updated.

--- DimonSmart.WebScraper.Console/Program.cs
+++ DimonSmart.WebScraper.Console/Program.cs
@@ -18,7 +18,7 @@ namespace DimonSmart.WebScraper.Console
 
             try
             {
-                await Host.CreateDefaultBuilder(args)
+                var host = Host.CreateDefaultBuilder(args)
                     .UseContentRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!)
                     .UseSerilog()
                     .ConfigureAppConfiguration((context, config) =>
@@ -28,25 +28,39 @@ namespace DimonSmart.WebScraper.Console
                         config.AddEnvironmentVariables();
                         config.AddCommandLine(args);
                     })
+                    .ConfigureLogging(logging =>
+                    {
+                        logging.ClearProviders();
+                        logging.AddSerilog();
+                        logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Debug);
+                    })
                     .ConfigureServices((hostContext, services) =>
                     {
                         var configuration = hostContext.Configuration;
 
                         services
-                            .AddHostedService<ConsoleHostedService>()
+                            .AddSingleton<IHostedService, ConsoleHostedService>()
                             .AddTransient<IPageDownloader, PageDownloader>()
                             .AddSingleton<ILinkExtractor, LinkExtractor>()
-                            .AddSingleton<IPageStorage, PageStorage>()
+                            .AddScoped<IPageStorage, PageStorage>()
                             .AddSingleton<IUrlQueueManager, UrlQueueManager>()
-                            .AddSingleton<WebScraper>()
-                            .AddSingleton<IUrlRepository, FileUrlRepository>()
-                            .AddDbContext<AppDbContext>(options => options.UseSqlite(configuration.GetConnectionString("FileStorageDb"), b => b.MigrationsAssembly("DimonSmart.WebScraper")))
+                            .AddScoped<WebScraper>()
+                            .AddScoped<IUrlRepository, FileUrlRepository>()
+                            .AddDbContext<AppDbContext>(options => options.UseSqlite(configuration.GetConnectionString("FileStorageDb"), b => b.MigrationsAssembly("DimonSmart.WebScraper")), ServiceLifetime.Transient)
                             .Configure<WebScraperSettings>(configuration.GetSection("WebScraperSettings"))
                             .Configure<StorageSettings>(configuration.GetSection("StorageSettings"));
                         // Register Serilog.ILogger
                         services.AddSingleton(Log.Logger);
                     })
-                    .RunConsoleAsync();
+                    .Build();
+
+                using (var scope = host.Services.CreateScope())
+                {
+                    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                    dbContext.Database.Migrate();
+                }
+
+                await host.RunAsync();
             }
             finally
             {
