98d35650a0f80a0195c6e3f8de1d805e80925bde
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2024-09-25 22:21:18 +0200

    SQL DB Suppoort added

---
+++ DimonSmart.WebScraper/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace DimonSmart.WebScraper
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<FileRecord> Files { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FileRecord>().HasKey(f => f.Id);
+        }
+    }
+}
